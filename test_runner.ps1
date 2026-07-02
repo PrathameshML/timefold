55941889413288cd739ea811782cdfe616bb8be1
Prathamesh Mali <prathameshm660@gmail.com>
2026-07-02 15:18:36 +0530

    deleted testing files and benchmark reports

--- test_runner.ps1
+++
@@ -1,8 +0,0 @@
-param($payloadFile)
-$uri = "http://localhost:8083/api/v2/shifts/assign"
-$headers = @{
-    "Content-Type" = "application/json"
-}
-$body = Get-Content -Raw -Path $payloadFile
-$response = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body $body
-$response | ConvertTo-Json -Depth 10
