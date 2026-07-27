2a23cc3ed0660da8718e68a350c68d13fd084306
Prathamesh Mali <prathameshm660@gmail.com>
2026-07-27 15:00:57 +0530

    deleted test and benchamark files

--- TestTime.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalTime;
-public class TestTime {
-    public static void main(String[] args) {
-        LocalTime t1 = LocalTime.parse("00:00");
-        System.out.println("t1 equals MIDNIGHT: " + t1.equals(LocalTime.MIDNIGHT));
-        System.out.println("t1: " + t1);
-    }
-}
